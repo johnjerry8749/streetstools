6651d21ab0354ef151887ccb569b8f878be33c0b
John <johnjerry8749@gmail.com>
2026-04-04 01:02:30 +0100

    Update api index configuration for Vercel

--- api/index.js
+++ api/index.js
@@ -35,7 +35,7 @@ app.use('/api/auth', authRouter);
 app.use('/downloads', express.static('public/downloads'));
 
 
-app.get("/index.js", (req, res) => {
+app.get("/api/index.js", (req, res) => {
   res.json({ message: "Backend working!" });
 });
 
